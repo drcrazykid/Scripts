9837a9194b838387e15ece27a92cbaecc5868d4c
CJ <43323243+cdwhitejr@users.noreply.github.com>
2021-02-11 00:16:43 +0000

    added in future add-on support comments

--- weekly-update.py
+++ weekly-update.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
+
+# Updating Debian Based Linux
 import os,sys,subprocess
 from subprocess import Popen, PIPE
 from datetime import datetime as dt
+
+
+# Add in support to handle different managers: apt, pacman, yum, dnf, etc.
 
 currentTime = dt.now()
 currentDate = dt.date(dt.now())
 update_tuple = ('sudo', 'apt', 'update')
 upgrade_tuple = ('sudo','apt','upgrade','-y')
 fullUpgrade_tuple = ('sudo','apt','full-upgrade','-y')
-#subprocess.call(update_tuple)
-#subprocess.call(upgrade_tuple)
-#subprocess.call(fullUpgrade_tuple)
+
+# Add in conditional statement, read '/etc/*-release
+# For Updating pihole
+piHole_tuple = ('sudo','pihole','-up')
+
 
 p1 = Popen(update_tuple,stdin=PIPE,stdout=PIPE,stderr=PIPE)
 output1, err1 = p1.communicate()
@@ -20,6 +27,11 @@ output2, err2 = p2.communicate()
 
 p3 = Popen(fullUpgrade_tuple,stdin=PIPE,stdout=PIPE,stderr=PIPE)
 output3, err3 = p3.communicate()
+
+# For updating pihole
+p4 = Popen(piHole_tuple,stdin=PIPE,stdout=PIPE,stderr=PIPE)
+output4, err4 = p4.communicate()
+
 
 log_path = '/home/pi/Documents/update_logs'
 
@@ -32,9 +44,16 @@ except FileNotFoundError as e:
 
 with open('Log: {}'.format(str(currentDate)),'w') as f:
     f.write('Conducted update, upgrade and full-upgrade check on '+str(currentTime)+'\n')
+    
     f.write('\n\nUpdate Output\n'+'='*50+'\n')
     f.write(output1.decode('utf-8'))
+    
     f.write('\n\nUpgrade Output\n'+'='*50+'\n')
     f.write(output2.decode('utf-8'))
+    
     f.write('\n\nFull-Upgrade Output\n'+'='*50+'\n')
     f.write(output3.decode('utf-8'))
+
+    # For updating pihole
+    f.write('\n\nPihole Upadte/Upgrade Output\n' + '='*50+'\n')
+    f.write(output4.decode('utf-8'))
