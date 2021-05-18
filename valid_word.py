d0a8addc6514293624ed4473635c7e33dd7b2991
drcrazykid <43323243+drcrazykid@users.noreply.github.com>
2021-05-18 23:19:36 +0100

    update

--- valid_word.py
+++ valid_word.py
@@ -10,79 +10,80 @@ class MyProgram():
     def __init__(self):
         self.d = enchant.Dict("en_US")
 
-listOfLetters = []
+        self.listOfLetters = []
 
-def CreateFilename():
-    temp = input("What is the name of this file (without the extension)? ")
-    filename = temp + ".txt"
-    print("Here is your filename:",filename)
-    return filename
+    def CreateFilename(self):
+        temp = input("What is the name of this file (without the extension)? ")
+        filename = temp + ".txt"
+        print("Here is your filename:",filename)
+        return filename
 
-def CreateListOfLetters(theInput):
-    letters = []
-    for l in theInput:
-        letters.append(l)
-    return letters
+    def CreateListOfLetters(self, theInput):
+        letters = []
+        for l in theInput:
+            letters.append(l)
+        return letters
 
-def CheckForFile(theFilename):
-    if (os.path.exists(theFilename)):
-        theFilename = theFilename.replace(".","-01.")
-        return theFilename
-    else:
-        return theFilename
+    def CheckForFile(self, theFilename):
+        if (os.path.exists(theFilename)):
+            theFilename = theFilename.replace(".","-01.")
+            return theFilename
+        else:
+            return theFilename
 
 
-def CreatePossibleWords(listOfLetters,filename):
-    x = 2
-    listOfWords = []
-    while x < len(listOfLetters):
-        print("Creating",x,"letter words")
-        tempList = [''.join(comb) for comb in itertools.product(listOfLetters, repeat=x)]
-        
-        # Create a text file with all possible words
-        for word in tempList:
-            if d.check(word):
-                with open(filename,'a+') as f:
-                    f.writelines(word+"\n")
+    def CreatePossibleWords(self, listOfLetters,filename):
+        x = 2
+        listOfWords = []
+        while x < len(listOfLetters):
+            print("Creating",x,"letter words")
+            tempList = [''.join(comb) for comb in itertools.product(listOfLetters, repeat=x)]
+            
+            # Create a text file with all possible words
+            for word in tempList:
+                if self.d.check(word):
+                    with open(filename,'a+') as f:
+                        f.writelines(word+"\n")
 
-        listOfWords += tempList
-        x +=1    
-        
-    return listOfWords
+            listOfWords += tempList
+            x +=1    
+            
+        return listOfWords
 
-def ReadFromFile(filename):
-    listOfWords = []
-    with open(filename,'r') as f:
-        listOfWords = f.readlines()
-    x = 0
-    while (x < len(listOfWords)):
-        listOfWords[x] = listOfWords[x].replace('\n','')
-        x +=1
-    return listOfWords
+    def ReadFromFile(self, filename):
+        listOfWords = []
+        with open(filename,'r') as f:
+            listOfWords = f.readlines()
+        x = 0
+        while (x < len(listOfWords)):
+            listOfWords[x] = listOfWords[x].replace('\n','')
+            x +=1
+        return listOfWords
 
-def CheckIfValid(word):
+    def CheckIfValid(self, word):
 
-    if(d.check(word)):
-        print(word)
-
-def CheckListForValid(wordList):
-    count = 0
-    for word in wordList:
-        if(d.check(word)):
-            count +=1
+        if(self.d.check(word)):
             print(word)
-    print(count,"valid words")
+
+    def CheckListForValid(self, wordList):
+        count = 0
+        for word in wordList:
+            if(self.d.check(word)):
+                count +=1
+                print(word)
+        print(count,"valid words")
 #CreatePossibleWords(listOfLetters,"Test File.txt")
 
 theInput = input("Please type the letters with no spaces: ")
+prog = MyProgram()
 
-filename = CreateFilename()
+filename = prog.CreateFilename()
 
-listOfLetters = CreateListOfLetters(theInput)
+listOfLetters = prog.CreateListOfLetters(theInput)
 
-CreatePossibleWords(listOfLetters,filename)
+prog.CreatePossibleWords(listOfLetters,filename)
 
-possibleWords = ReadFromFile(filename)
+possibleWords = prog.ReadFromFile(filename)
 
 print(possibleWords)
 print("Complete")
