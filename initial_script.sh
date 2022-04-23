fb596ea73e15fcf1221ea037f3b3320693ed5b67
drcrazykid <43323243+drcrazykid@users.noreply.github.com>
2022-04-23 01:31:39 -0400

    major changes to initial script

--- initial_script.sh
+++ initial_script.sh
@@ -8,7 +8,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install terminator git zsh curl
+sudo apt install git zsh curl vim -y
 
 
 
@@ -21,7 +21,7 @@ bash $FOLDER/install_Ohmyzsh.sh
 
 sudo echo '
 if [ -d "$HOME/.bin" ] ; then
-    PATH="$HOME/bin:$PATH"
+    PATH="$HOME/.bin:$PATH"
 fi' >> /etc/zsh/zprofile
 
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
@@ -37,9 +37,29 @@ cp ./.zsh_aliases ~/.zsh_aliases
 
 bash ./mod-vimrc.sh
 
-mkdir ~/.bin
+if [ -d "$HOME/.bin" ]
+then
+    echo "'.bin' already exists."
+else
+    mkdir ~/.bin
+fi 
+
 cp $FOLDER/weekly-update.py ~/.bin/weekly-update.py
-cp $FOLDER/update-all
+cp $FOLDER/update-all ~/.bin/update-all
 
 cd ~/Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ~/Downloads/google.deb
+
+# Run mod-vimrc.sh
+bash ./mod-vimrc.sh
+
+# Install terminator with configs
+/usr/bin/python3 ./install_terminator_wConfigs.py
+
+# install Ohmyzsh
+bash ./install_Ohmyzsh.sh
+
+# Install VS Code
+bash ./install_code.sh
+
+
