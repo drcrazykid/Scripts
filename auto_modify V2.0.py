475437d9f3704f6cd2c4e396042cb3e8cda069b2
drcrazykid <43323243+drcrazykid@users.noreply.github.com>
2022-12-20 03:20:57 -0500

    new

---
+++ auto_modify V2.0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import os
+
+
+
+print(wos.path.abspath(os.curdir))
+
+# list of things to remove 'axxo'
+remove_list = []
+
+
+# get the folder location to do the automatic name changes
+def getfilepath():
+    pass
+
+# Whereever there are periods change to spaces except for the last period located in the file name
