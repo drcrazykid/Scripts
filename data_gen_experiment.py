2494ce6d36b6e26a01b9a5e7227e0f619094bf62
CJ White <cdwhitejr@gmail.com>
2023-08-27 22:44:34 -0400

    update

--- data_gen_experiment.py
+++ data_gen_experiment.py
@@ -5,10 +5,28 @@
 # Fields: First Name, Last name, Age, occupation, marital status, salary, kids (under 18), more to come
 import pandas as pd
 import numpy as np
+from random import randint
 
-
-ages_numpy = np.random.randint(18,70,200)
+data_points = 200
+ages_numpy = np.random.randint(18,70,data_points)
 ages_series = pd.Series(data=ages_numpy,name='Ages')
 
 
+
+def create_marriage_data(size=200):
+    marital_status_choices = ["S","M","D"]
+    marital_status_data = []
+    x = 0
+    data = []
+    while x < size:
+        choice = randint(0,2)
+        data.append(marital_status_choices[choice])
+        x+=1
+
+    return data    
+
+m_data = create_marriage_data()
+marital_status_series = pd.Series(data=m_data,name="Marital Status")
+
 print(ages_series)
+print(marital_status_series)
