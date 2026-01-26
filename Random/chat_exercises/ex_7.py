f5704d8c639c849f8a30adbaef834bcd4d005f0b
CJ <cdwhitejr@gmail.com>
2026-01-26 01:17:29 -0600

    feat: add threading and multiprocessing imports for chat exercises

---
+++ Random/chat_exercises/ex_7.py
@@ -0,0 +1,14 @@
+'''
+Exercise 7: Producer / Consumer (Multiprocessing) Goal: One process
+produces work items, multiple processes consume them.
+
+Concepts: - Work queues - Process coordination
+
+Constraints: - Producer sends 50 tasks - Consumers process until queue
+empty
+
+Hint: Use None as a sentinel value.
+'''
+
+from threading import Thread
+from multiprocessing import Process, Queue
